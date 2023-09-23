7e9e5b5da50f5f168bb9c024eb0062bfff6ac716
TommcyOWO <tangminghong.tom@gmail.com>
2023-09-23 22:40:35 +0800

    update

---
+++ backend/main.py
@@ -0,0 +1,110 @@
+from fastapi import Depends, FastAPI,HTTPException,Request,Response
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
+import json
+import math
+
+#過濾器
+from model import *
+
+#oauth 初始化
+from oauth import *
+
+#初始化
+limiter = Limiter(key_func=get_remote_address)
+app = FastAPI(redoc_url=None)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+origins = [
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#阻擋request
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"ERROR":f"Too many request,{exc.detail}"},
+    )
+
+#404 error handler
+@app.exception_handler(404)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"ERROR":exc.detail},
+    )
+
+#405 error handler
+@app.exception_handler(405)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"ERROR":exc.detail},
+    )
+
+#main
+#oauth path
+@app.post("/register")
+async def register_user(user: User):
+    with open(path + 'count.json', 'r') as file:
+        users_db = json.load(file)
+    if user.username in users_db:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    hashed_password = password_context.hash(user.password)
+    users_db[user.username] = {
+        "email": user.email,
+        "username": user.username,
+        "password": hashed_password
+    }
+    with open(path + 'count.json', 'w') as file:
+        json.dump(users_db, file, indent=4)
+    return {"message": "User registered successfully"}
+
+@app.post("/reset")
+async def reset_acount(user:User):
+    with open(path+'count.json','r') as file:
+        reset_acount = json.load(file)
+    if user.username not in reset_acount:
+        raise HTTPException(status_code=400, detail="Username not exists.")
+    if user.email != reset_acount[user.username]['email']:
+        raise HTTPException(status_code=400, detail="Email do not match.")
+    hashed_password = password_context.hash(user.password)
+    with open(path+'count.json','w') as file:
+        reset_acount[user.username]["password"] = hashed_password
+        json.dump(reset_acount,file,indent=4)
+    return {"message":"Reset done"}
+
+@app.post("/logon")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    with open(path+'count.json','r') as file:
+        users_db = json.load(file)
+    if form_data.username not in users_db:
+        raise HTTPException(status_code=400, detail="Wrong password or username")
+    user = users_db[form_data.username]
+    if user is None or not password_context.verify(form_data.password, user["password"]):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    access_token = create_access_token(data={"sub": form_data.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="192.168.1.110",port=5000)
+    #uvicorn.run(app,host='0.0.0.0',ssl_keyfile='./key.pem',ssl_certfile='./cert.pem')
---
+++ backend/oauth.py
@@ -0,0 +1,34 @@
+from fastapi.security import OAuth2PasswordBearer
+
+from datetime import datetime, timedelta
+
+import jwt
+from jwt import PyJWTError
+from passlib.context import CryptContext
+
+from model import *
+
+path = './db/'
+    
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = "46936036427095cb76ed002fe7a168c24ee815003b5e5416916cd718f53a9b5d"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        return username
+    except PyJWTError:
+        return None
